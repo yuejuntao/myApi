ea8f0d55b166b32312adfe4603b52a983a594d01
yuejuntao <254647202@qq.com>
2019-01-05 13:30:09 +0800

    搭建api平台

---
+++ src/main/java/com/dh/yjt/myApi/MyApiApplication.java
@@ -0,0 +1,14 @@
+package com.dh.yjt.myApi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyApiApplication.class, args);
+	}
+
+}
+
